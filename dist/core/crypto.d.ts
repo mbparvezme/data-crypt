4081d48b01a5e28781596b88c6ab19242361e7e9
M B Parvez (Ron) <mbparvezme@gmail.com>
2025-11-25 12:18:51 +0600

    version and .gitignore updated

--- dist/core/crypto.d.ts
+++
@@ -1,4 +0,0 @@
-import { DeriveOptions } from './types.js';
-export declare const deriveKey: (password: string, salt: Uint8Array, opts: DeriveOptions) => Promise<CryptoKey>;
-export declare const encryptRaw: (key: CryptoKey, iv: Uint8Array, data: Uint8Array) => Promise<Uint8Array>;
-export declare const decryptRaw: (key: CryptoKey, iv: Uint8Array, data: Uint8Array) => Promise<Uint8Array>;
